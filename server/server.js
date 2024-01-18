d38fb6a996e7fce2e947a2e9b1c3de7b66090147
AlbertoN_03 <narbonaalb03@gmail.com>
2024-01-18 01:57:59 +0100

    desactivar cors

--- server/server.js
+++ server/server.js
@@ -40,9 +40,9 @@ conexion.connect((err) => {
 
   http
     .createServer((req, res) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', '*');
-      res.setHeader('Access-Control-Allow-Headers', '*');
+      // res.setHeader('Access-Control-Allow-Origin', '*');
+      // res.setHeader('Access-Control-Allow-Methods', '*');
+      // res.setHeader('Access-Control-Allow-Headers', '*');
 
       if (req.method === 'OPTIONS') {
         res.writeHead(200);
