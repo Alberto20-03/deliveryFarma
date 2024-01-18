4302f8fd718c3991f4be230d7fbdaef9f8b5e5ba
AlbertoN_03 <narbonaalb03@gmail.com>
2024-01-18 03:23:22 +0100

    options cors

--- server/server.js
+++ server/server.js
@@ -38,6 +38,12 @@ conexion.connect((err) => {
     throw err;
   }
 
+  const handleCors = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  };
+
   http
     .createServer((req, res) => {
       res.setHeader('Access-Control-Allow-Origin', 'https://delivery-farma.vercel.app');
@@ -45,6 +51,7 @@ conexion.connect((err) => {
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
       if (req.method === 'OPTIONS') {
+        handleCors(req, res);
         res.writeHead(200);
         res.end();
         return;
