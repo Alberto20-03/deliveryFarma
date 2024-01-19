b3def4777ccd1f0d5fdf27474d533487734cda28
AlbertoN_03 <narbonaalb03@gmail.com>
2024-01-19 12:26:34 +0100

    ,

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ conexion.connect((err) => {
     .createServer((req, res) => {
       res.setHeader(
         'Access-Control-Allow-Origin',
-        'https://delivery-farma.vercel.app'
+        '*'
       );
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
