fd0899d56618c9a7c41ccaa1233add23a6cf438c
AlbertoN_03 <narbonaalb03@gmail.com>
2024-02-11 14:24:10 +0100

    install cypress

--- client/src/components/bocadillo-dialogo/mensaje-chatgpt.jsx
+++ client/src/components/bocadillo-dialogo/mensaje-chatgpt.jsx
@@ -3,12 +3,11 @@ import { URL_APP } from '../../constantes';
 // componente que realiza una petición a la API con el mensaje del usuario
 export async function peticionMensajeChatGPT(mensaje) {
   const peticion = await fetch(`${URL_APP}/dialogo-chat`, {
-    method: 'POST',
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       accept: 'application/json',
     },
-    body: JSON.stringify(mensaje),
   });
   const json = await peticion.json();
   return await json.respuesta.message.content;
